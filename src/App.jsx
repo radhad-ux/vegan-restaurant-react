f307a0e16393b5b2c08ecb0b41990c1b340be4e2
radhad-ux <radhad83@gmail.com>
2022-03-08 13:46:28 +0100

    home page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Project files
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 import "./styles/style.css";
 
 function App() {
   return (
     <Router>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
