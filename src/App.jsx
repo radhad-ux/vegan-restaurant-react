fba76e15fbdaa850e237e875b977d2231907e107
radhad-ux <radhad83@gmail.com>
2022-03-08 13:31:53 +0100

    navbar component and css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Project files
 import NavBar from "./components/NavBar";
 import "./styles/style.css";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-    </div>
+    </Router>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LOGO from "../assets/logo.png";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="navbar_links">
+      <div className="navbar_left">
+        <Link to="/">
+          <img className="navbar_img" src={LOGO} alt="" />
+        </Link>
+        <h3 className="navbar_title">The Green Alley</h3>
+      </div>
+      <div className="navbar_right">
+        <Link to="/">Home</Link>
+        <Link to="/dishes">Dishes</Link>
+        <Link to="/desserts">Deserts</Link>
+
+        <Link to="/drinks">Drinks</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
 }
